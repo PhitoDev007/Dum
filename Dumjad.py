359b946a4173d3df869e4a985ddee84d76781ecf
Phito <pjmmr2525@gmail.com>
2025-11-25 02:54:44 +0000

    First

--- Dumjad.py
+++ Dumjad.py
@@ -1,30 +1,20 @@
-class PET:
-    def __init__(self, age, weight, gender):
-        self.age = age
-        self.weight = weight
-        self.gender = gender
-    def show_all_information(self):
-        print(f"Age: {self.age}")
-        print(f"Weight: {self.weight}")
-        print(f"Gender: {self.gender}")
-class Dog(PET):
-    def __init__(self, age, weight, gender, fang_size):
-        super().__init__(age, weight, gender)
-        self.fang_size = fang_size
-    def show_all_information(self):
-        super().show_all_information()
-        print(f"Fang size: {self.fang_size}")
-class Cat(PET):
-    def __init__(self, age, weight, gender, fang_size):
-        super().__init__(age, weight, gender)
-        self.fang_size = fang_size
-    def show_all_information(self):
-        super().show_all_information()
-        print(f"Fang size: {self.fang_size}")
-class Bird(PET):
-    def __init__(self, age, weight, gender, wing_size):
-        super().__init__(age, weight, gender)
-        self.wing_size = wing_size
-    def show_all_information(self):
-        super().show_all_information()
-        print(f"Wing size: {self.wing_size}")
+class Tomato():
+    def type(self):
+        print ("Vegetable")
+    def color(self):
+        print("Red")
+
+class Apple():
+    def type(self):
+        print("Fruit")
+    def color(self):
+        print ("Red")
+
+def func(obj):
+    obj. type ( )
+    obj . color ()
+
+obj_tomato = Tomato()
+obj_apple = Apple()
+func (obj_tomato)
+func(obj_apple)
