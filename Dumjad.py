3fd4e07fcf3703a961108d40ae53a49f2e2cc0fa
Phito <pjmmr2525@gmail.com>
2025-11-25 02:59:19 +0000

    hi

--- Dumjad.py
+++ Dumjad.py
@@ -1,19 +1,28 @@
-class India():
-    def capital(self):
-        print("New Delhi")
+class Bird:
+    def intro(self):
+        print("There are many types of birds.")
+    
+    def flight(self):
+        print("birds can fly.")
 
-    def language(self):
-        print("Hindi and English")
+class sparrow(Bird):
+    def flight(self):
+        print("Sparrows can fly.")
 
-class USA():
-    def capital(self):
-        print("Washington, D.C.")
+class ostrich(Bird):
+    pass
+    # def flight(self):
+    #     print("Ostriches cannot fly.")
 
-    def language(self):
-        print("English")
+obj_bird = Bird()
+obj_spr = sparrow()
+obj_ost = ostrich()
 
-obj_ind = India()
-obj_usa = USA()
-for country in (obj_ind, obj_usa):
-    country. capital()
-    country. language ()
+obj_bird.intro()
+obj_bird.flight()
+
+obj_spr.intro()
+obj_spr.flight()
+
+obj_ost.intro()
+obj_ost.flight()
