3198876d896081d23ff0928ba8e2474dfa4e2402
Phito <pjmmr2525@gmail.com>
2025-11-25 03:02:49 +0000

    hi

--- Dumjad.py
+++ Dumjad.py
@@ -1,40 +1,44 @@
-from math import pi
 
-class Shape:
-    def __init__(self, name):
-        self.name = name
+from abc import ABC, abstractmethod
+
+class Car(ABC):
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
     
-    def area(self):
+    @abstractmethod
+    def printDetails(self):
         pass
     
-    def fact(self):
-        return "I am a two-dimensional shape."
-    
-    def __str__(self):
-        return self.name
 
-class Square(Shape):
-    def __init__(self, length):
-        super().__init__("Square")
-        self.length = length
+    def accelerate(self):
+        print("speed up ...")
     
-    def area(self):
-        return self.length**2
-    
-    def fact(self):
-        return "Squares have each angle equal to 90 degrees."
+    def break_applied(self):
+        print("Car stop")
 
-class Circle(Shape):
-    def __init__(self, radius):
-        super().__init__("Circle")
-        self.radius = radius
-    
-    def area(self):
-        return pi*self.radius**2
 
-a = Square(4)
-b = Circle(7)
-print(b)
-print(b.fact())
-print(a.fact())
-print(b.area())
+class Hatchback(Car):
+    def printDetails(self):
+        print("Brand:", self.brand)
+        print("Model:", self.model)
+        print("Year:", self.year)
+    
+    def Sunroof(self):
+        print("Not having this feature")
+
+
+class Suv(Car):
+    def printDetails(self):
+        print("Brand:", self.brand)
+        print("Model:", self.model)
+        print("Year:", self.year)
+    
+    def Sunroof(self):
+        print("Available")
+
+car1 = Hatchback("Maruti", "Alto", "2022")
+
+car1.printDetails()
+car1.accelerate()
